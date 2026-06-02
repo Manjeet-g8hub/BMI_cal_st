2702bddfef2c60fe52a64042d36998e2138ede26
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-02 16:06:43 +0530

    Enhance BMI calculator with user name and new prompts
    Updated the BMI calculator to include user name input and improved prompt for nutrition advice.

--- BMI_cal_st.py
+++ BMI_cal_st.py
@@ -3,11 +3,15 @@ import google.genai as genai
 
 google_api_key = st.secrets["google"]["api_key"]
 
-client = genai.Client(api_key=google_api_key)
+client = genai.Client(api_key = google_api_key)
 
+st.title("Welcome to the AI Health Assistant")
 st.title("BMI Calculator with AI Nutritionist")
 
+st.title("_AI_ is :blue[cool] :sunglasses:")
+
 # Input fields for height and weight
+name = st.text_input("Enter Your name", key="name")
 ht = st.slider("Enter your height in meters:", min_value=1.0, max_value=2.5, step=0.01)
 wt = st.slider("Enter your weight in kilograms:", min_value=1.0, max_value=300.0, step=0.1)
 gender = st.selectbox("Select your gender:", ["Male", "Female"])
@@ -16,11 +20,11 @@ gender = st.selectbox("Select your gender:", ["Male", "Female"])
 bmi = wt / (ht ** 2)
 st.write(f"Your BMI is: {bmi:.2f}")
 
-prompt = f"Act like an expert nutritionist, comment on the BMI with the following data: height as {ht}, weight as {wt}, and BMI as {bmi}"
+prompt = f"Great {name} and Act like an expert nutritionist, evaluate the {bmi} and share a diet chart"
 
 # Generate content from Gemini
 response = client.models.generate_content(
-    model="gemini-3.5-flash",
+    model="gemini-2.5-flash",
     contents= prompt)
 
 st.write(response.text)
