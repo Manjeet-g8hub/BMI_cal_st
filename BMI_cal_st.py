e5fe4bdd138348c0bed7837fce4408bffb372f0c
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-02 16:39:41 +0530

    Refactor BMI calculator code for clarity and structure

--- BMI_cal_st.py
+++ BMI_cal_st.py
@@ -12,7 +12,7 @@ st.title("_AI_ is :blue[cool] :sunglasses:")
 
 # Input fields for height and weight
 name = st.text_input("Enter Your name", key="name")
-ht = st.slider("Enter your height in meters:", min_value=1.0, max_value=2.5, step=0.01)
+ht = st.slider("Enter your height in meters:", min_value=1.0, max_value= 2.5, step=0.1)
 wt = st.slider("Enter your weight in kilograms:", min_value=1.0, max_value=300.0, step=0.1)
 gender = st.selectbox("Select your gender:", ["Male", "Female"])
 
