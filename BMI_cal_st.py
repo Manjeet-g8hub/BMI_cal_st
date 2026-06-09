87c686e0a26723880a43cae677b9e466f09b909f
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-09 13:09:20 +0530

    Update BMI_cal_st.py

--- BMI_cal_st.py
+++ BMI_cal_st.py
@@ -1,34 +1,34 @@
-import streamlit as st
-import google.genai as genai
+try:
+    import streamlit as st
+    import google.genai as genai
+    
+    google_api_key = st.secrets["google"]["api_key"]
+    client = genai.Client(api_key = google_api_key)
+    
+    st.title("BMI Calculator with AI Nutritionist")
+    st.title("Your Personalised AI Health Assistant")
+    
+    st.title("_AI_ is :blue[cool] :sunglasses:")
+    # Input fields for height and weight
+    name = st.text_input("* Enter Your name", key="name")
+    ht = st.slider("Enter your height in meters:", min_value=1.0, max_value= 2.5, step=0.01)
+    wt = st.slider("Enter your weight in kilograms:", min_value=1.0, max_value=300.0, step=0.1)
+    gender = st.selectbox("Select your gender:", ["Male", "Female"])
 
-google_api_key = st.secrets["google"]["api_key"]
-
-client = genai.Client(api_key = google_api_key)
-
-st.title("BMI Calculator with AI Nutritionist")
-st.title("Your Personalised AI Health Assistant")
-
-st.title("_AI_ is :blue[cool] :sunglasses:")
-
-
-# Input fields for height and weight
-name = st.text_input("* Enter Your name", key="name")
-ht = st.slider("Enter your height in meters:", min_value=1.0, max_value= 2.5, step=0.01)
-wt = st.slider("Enter your weight in kilograms:", min_value=1.0, max_value=300.0, step=0.1)
-gender = st.selectbox("Select your gender:", ["Male", "Female"])
-
-# Calculate BMI
-if st.button("Calculate BMI"):
-    if not name.strip():
-        st.error("Please enter your name.")
+    # Calculate BMI
+    if st.button("Calculate BMI"):
+        if not name.strip():
+            st.error("Please enter your name.")
         
-    else:
-        bmi = wt / (ht ** 2)
-        st.write(f"Your BMI is: {bmi:.2f}")
-        prompt = f"Great {name} and Act like an expert nutritionist, evaluate the {bmi} and share a diet chart"
+        else:
+            bmi = wt / (ht ** 2)
+            st.write(f"Your BMI is: {bmi:.2f}")
+            prompt = f"Great {name} and Act like an expert nutritionist, evaluate the {bmi} and share a diet chart"
         
-        # Generate content from Gemini
-        response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents= prompt)
-        st.write(response.text)
+            # Generate content from Gemini
+            response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents= prompt)
+            st.write(response.text)
+
+except:
